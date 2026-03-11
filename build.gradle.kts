78f1d91f4b03569c1bddb757753d2ba065b2a4e2
Sully Millet <119430426+SullyMLT@users.noreply.github.com>
2026-03-11 08:38:55 +0100

    test CI jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "5.1.0.4882"
+    id("jacoco") // pour la couverture de code
 }
 
 group = "org.example"
@@ -17,4 +19,31 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "2D3D")
+        property("sonar.projectName", "2D3D")
+        property("sonar.host.url", "http://localhost:9000")
+        // la variable d'env SONAR_TOKEN
+    }
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "main.java.com.ubo.tp.message.MessageAppLauncher"
+    }
+    from(configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
